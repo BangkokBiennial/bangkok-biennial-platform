7f718c05c8e3660fd8d0af68dff6c7997a4477be
dodadoa <maddodadoa@gmail.com>
2020-06-13 11:11:06 +0700

    clean up, changed all methods of redirecting after login

--- src/components/scenes/Auth/molecules/SignInGoogle.jsx
+++ src/components/scenes/Auth/molecules/SignInGoogle.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { navigate } from 'gatsby';
 
 import { withFirebase } from '../../../../utils/Firebase';
-import { HOME } from '../../../../constants/routes';
+import { PAVILION_INFO_REGISTER } from '../../../../constants/routes';
 
 import { FaGoogle } from 'react-icons/fa';
 
@@ -37,7 +37,7 @@ class SignInGoogle extends Component {
       })
       .then(() => {
         this.setState({ error: null });
-        navigate(HOME);
+        navigate(PAVILION_INFO_REGISTER);
       })
       .catch(error => {
         if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
