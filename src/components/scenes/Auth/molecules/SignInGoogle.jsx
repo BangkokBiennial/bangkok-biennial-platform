c39f7f15c0a19f2340f0225834ca274760d1d3b2
dodadoa <maddodadoa@gmail.com>
2020-07-06 05:36:52 +0700

    refactor singin google

--- src/components/scenes/Auth/molecules/SignInGoogle.jsx
+++ src/components/scenes/Auth/molecules/SignInGoogle.jsx
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { navigate } from 'gatsby';
 
-import { withFirebase } from '../../../../utils/Firebase';
 import { PAVILION_INFO_REGISTER, REGISTRATION_STATUS, PAVILION_DETAIL_REGISTER } from '../../../../constants/routes';
 import RegistrationStatus from '../../../../constants/registrationStatus';
 
@@ -18,29 +17,25 @@ const ERROR_MSG_ACCOUNT_EXISTS = `
   your personal account page.
 `;
 
-class SignInGoogle extends Component {
-  constructor(props) {
-    super(props);
+const SignInGoogle = ({ firebase }) => {
+  const [error, setError] = useState(null)
 
-    this.state = { error: null };
-  }
-
-  onSubmit = async event => {
+  const onSubmit = async event => {
     event.preventDefault();
     try {
-      const socialAuthUser = await this.props.firebase
+      const socialAuthUser = await firebase
         .doSignInWithGoogle()
-      const userSnapshot = await this.props.firebase.getUser(socialAuthUser.user.uid)
+      const userSnapshot = await firebase.getUser(socialAuthUser.user.uid)
       const user = userSnapshot.data()
       const status = user ? user.registrationStatus : RegistrationStatus.NEW_USER
-      await this.props.firebase.user(socialAuthUser.user.uid)
+      await firebase.user(socialAuthUser.user.uid)
         .set({
           username: socialAuthUser.user.displayName,
           email: socialAuthUser.user.email,
           roles: 'user',
           registrationStatus: status
         });
-      await this.setState({ error: null });
+      await setError(null);
       
       switch (status) {
         case RegistrationStatus.FINISHED_BASIC:
@@ -56,28 +51,24 @@ class SignInGoogle extends Component {
       if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
         error.message = ERROR_MSG_ACCOUNT_EXISTS;
       }
-      this.setState({ error });
+      setError(error)
     }
     event.preventDefault();
   };
 
-  render() {
-    const { error } = this.state;
-
-    return (
-      <div className="login__content__providers__container">
-        <form
-          className="login__content__providers__item login__content__providers__item--google"
-          onSubmit={this.onSubmit}
-        >
-          <button type="submit">
-            <FaGoogle />
-          </button>
-        </form>
-        { error &&  <p style={{ color: '#FC0000' }}>{error.message}</p> }
-      </div>
-    );
-  }
+  return (
+    <div className="login__content__providers__container">
+      <form
+        className="login__content__providers__item login__content__providers__item--google"
+        onSubmit={onSubmit}
+      >
+        <button type="submit">
+          <FaGoogle />
+        </button>
+      </form>
+      { error &&  <p style={{ color: '#FC0000' }}>{error.message}</p> }
+    </div>
+  );
 }
 
-export default withFirebase(SignInGoogle);
+export default SignInGoogle
--- src/components/scenes/Auth/scenes/SignIn/SignIn.jsx
+++ src/components/scenes/Auth/scenes/SignIn/SignIn.jsx
@@ -1,58 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SignInForm from '../../molecules/SignInForm';
 import SignInGoogle from '../../molecules/SignInGoogle';
-import SignInFacebook from '../../molecules/SignInFacebook';
+import { withFirebase } from '../../../../../utils/Firebase';
 import SignUpLink from '../../scenes/SignUp/atoms/SignUpLink';
 import PasswordForgetLink from '../../../../molecules/PasswordForget/atoms/PasswordForgetLink';
+import Loading from '../../../../atoms/Loading'
 
-class SignIn extends Component {
-  render() {
+const SignIn = ({ firebase }) => {
+  const [_initFirebase, setInitFirebase] = useState(false)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (firebase && !_initFirebase) {
+      setInitFirebase(true)
+      setLoading(false)
+    }
+  }, [firebase])
+
+  if (loading) {
     return (
-      <div className="login">
-        <div className="login__content">
-          <div className="login__content__inner">
-            <div className="login__content__header">
-              <div className="login__content__header__title">
-                Sign in to Bangkok Biennial Platform
+      <div className="home container">
+         <Loading />
+      </div>
+    )
+  }
+
+  return (
+    <div className="login">
+      <div className="login__content">
+        <div className="login__content__inner">
+          <div className="login__content__header">
+            <div className="login__content__header__title">
+              Sign in to Bangkok Biennial Platform
+            </div>
+          </div>
+          <div className="login__content__login-form">
+            <div className="login__content__form">
+              <SignInForm />
+            </div>
+            <div className="login__content__password-forget">
+              <PasswordForgetLink />
+            </div>
+            <div className="login__content__or">
+              <span className="login__content__or__line" />
+              <span className="login__content__or__text">or</span>
+              <span className="login__content__or__line" />
+            </div>
+            <div className="login__content__providers register__content__providers">
+                <SignInGoogle firebase={firebase} />
+            </div>{' '}
+            <div className="login__content__creator">
+              <div className="login__content__creator__inner">
+                <p>This project is an opensource.</p>
+                <iframe
+                  src="https://ghbtns.com/github-btn.html?user=BangkokBiennial&repo=bangkok-biennial-platform&type=star&count=true"
+                  frameborder="0"
+                  scrolling="0"
+                  width="80px"
+                  height="20px"
+                />
               </div>
             </div>
-            <div className="login__content__login-form">
-              <div className="login__content__form">
-                <SignInForm />
-              </div>
-              <div className="login__content__password-forget">
-                <PasswordForgetLink />
-              </div>
-              <div className="login__content__or">
-                <span className="login__content__or__line" />
-                <span className="login__content__or__text">or</span>
-                <span className="login__content__or__line" />
-              </div>
-              <div className="login__content__providers register__content__providers">
-                  <SignInGoogle />
-              </div>{' '}
-              <div className="login__content__creator">
-                <div className="login__content__creator__inner">
-                  <p>This project is an opensource.</p>
-                  <iframe
-                    src="https://ghbtns.com/github-btn.html?user=BangkokBiennial&repo=bangkok-biennial-platform&type=star&count=true"
-                    frameborder="0"
-                    scrolling="0"
-                    width="80px"
-                    height="20px"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="login__content__register">
-              <SignUpLink isRegister />
-            </div>
+          </div>
+          <div className="login__content__register">
+            <SignUpLink isRegister />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default SignIn;
+export default withFirebase(SignIn);
