fb91dfffee6f2b6511a9c1b046999180a1fe03f2
dodadoa <maddodadoa@gmail.com>
2020-08-09 18:28:25 +0700

    add the date launch

--- src/components/atoms/CheckBox.jsx
+++ src/components/atoms/CheckBox.jsx
@@ -7,7 +7,9 @@ const CheckBox = ({
   onClick,
   labelName,
   className,
-  required
+  required,
+  staticLabel,
+  disabled
 }) => {
   return (
     <div className={classNames('checkbox', className)}>
@@ -15,15 +17,19 @@ const CheckBox = ({
         {required && <div className="checkbox__label__asterisk">*</div>}
         <label className="checkbox__label__text">{labelName}</label>
       </div>
-      <div className="checkbox__element__container">
+      <div className={classNames("checkbox__element__container", disabled && "disabled")}>
         <div
-          onClick={onClick}
+          onClick={disabled ? () => {} : onClick}
           className={value ? "checkbox__element__checked" : "checkbox__element__unchecked"}
         >
           {value ? <FiCheck className="checkbox__element__check-icon"/> : null}
         </div>
         <p className="checkbox__label__value">
-          { value ? 'yes' : 'no' }
+          { 
+            staticLabel
+              ? staticLabel
+              : value ? 'yes' : 'no' 
+          }
         </p>
       </div>
     </div>
--- src/components/scenes/PavilionRegistration/PavilionRegistrationStatus.jsx
+++ src/components/scenes/PavilionRegistration/PavilionRegistrationStatus.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { withFirebase } from '../../../utils/Firebase'
+import { useToasts } from 'react-toast-notifications'
 import Loading from '../../atoms/Loading'
+import ClipLoader from "react-spinners/ClipLoader";
 import RegistrationStatus from '../../../constants/registrationStatus'
 import { navigate } from 'gatsby';
+import CheckBox from '../../atoms/CheckBox'
 import Button from '../../atoms/Button';
 import { PAVILION_INFO_REGISTER, PAVILION_DETAIL_REGISTER, REGISTRATION_STATUS } from '../../../constants/routes';
 
 const PavilionRegistrationStatus = ({ firebase }) => {
+
+  const { addToast } = useToasts()
+
   const [_initFirebase, setInitFirebase] = useState(false)
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState({})
+  const [bbTakePlaced, setBbTakePlaced] = useState([])
+  const [loadSending, setLoadSending] = useState(false)
+  const [allowedToSubmit, setAllowedToSubmit] = useState(true)
 
   useEffect(() => {
     if (firebase && !_initFirebase) {
@@ -24,6 +33,10 @@ const PavilionRegistrationStatus = ({ firebase }) => {
         try {
           const userSnapshot = await firebase.getUser(firebase.getCurrentUserId())
           const user = userSnapshot.data()
+          if (user.dateLaunch) {
+            setBbTakePlaced(user.dateLaunch)
+            setAllowedToSubmit(false)
+          }
           await setUser(user)
           setLoading(false)
         } catch (error) {
@@ -65,6 +78,61 @@ const PavilionRegistrationStatus = ({ firebase }) => {
     }
   }
 
+  const dateLaunch = {
+    OCT31_TO_NOV21_2020: 'OCT31_TO_NOV21_2020',
+    MAR13_TO_APR3_2021: 'MAR13_TO_APR3_2021',
+    SEP17_TO_OCT9_2021: 'SEP17_TO_OCT9_2021',
+    NONE_OF_THE_ABOVE: 'NONE_OF_THE_ABOVE'
+  }
+
+  const isChecked = (date) => bbTakePlaced.find(el => el === date)
+  const toggle = (date) => {
+    isChecked(date)
+      ? setBbTakePlaced(bbTakePlaced.filter(el => el !== date))
+      : setBbTakePlaced([...bbTakePlaced, date])
+  }
+
+  const onSubmit = async () => {
+    setLoadSending(true)
+
+    try {
+      await firebase.updateUser(firebase.getCurrentUserId(), { dateLaunch: bbTakePlaced })
+      setLoadSending(false)
+      setAllowedToSubmit(false)
+      addToast('Thank you! the information is saved successfully.', { appearance: 'success' })
+    } catch (error) {
+      setLoadSending(false)
+      await addToast(`${error.message}`, { appearance: 'error', autoDismiss: false })
+    }
+  }
+
+  const renderCheckDateBox = () => (
+    <>
+      <p>In which period(s) of BB2020 will your Pavilion take place? select all that apply</p>
+      <CheckBox
+        disabled={!allowedToSubmit}
+        onClick={() => toggle(dateLaunch.OCT31_TO_NOV21_2020)}
+        value={isChecked(dateLaunch.OCT31_TO_NOV21_2020)}
+        staticLabel="Oct 31st-Nov 21st 2020"/>
+      <CheckBox
+        disabled={!allowedToSubmit}
+        onClick={() => toggle(dateLaunch.MAR13_TO_APR3_2021)}
+        value={isChecked(dateLaunch.MAR13_TO_APR3_2021)} 
+        staticLabel="March 13th-April 3rd 2021"/>
+      <CheckBox 
+        disabled={!allowedToSubmit}
+        onClick={() => toggle(dateLaunch.SEP17_TO_OCT9_2021)} 
+        value={isChecked(dateLaunch.SEP17_TO_OCT9_2021)} 
+        staticLabel="Sept 17th-Oct 9th  2021"/>
+      <CheckBox 
+        disabled={!allowedToSubmit}
+        onClick={() => toggle(dateLaunch.NONE_OF_THE_ABOVE)} 
+        value={isChecked(dateLaunch.NONE_OF_THE_ABOVE)} 
+        staticLabel="none of the above"/>
+      <Button disabled={!allowedToSubmit} onClick={onSubmit}>submit</Button>
+    </>
+  )
+
   return(
     <div className="home container">
       <div className="home__details">
@@ -81,6 +149,15 @@ const PavilionRegistrationStatus = ({ firebase }) => {
         user.registrationStatus === RegistrationStatus.FINISHED_ADVANCE
           && (
             <div className="pavilion-registration-status__waiting-for-approval">
+              <p className="pavilion-registration-status__cautious-text">May I have your attention, please ?</p>
+              <div className="pavilion-registration-status__date-box">
+                {
+                  loadSending
+                    ? <div style={{ position: 'relative', left: 'calc(50% - 17.5px)' }}><ClipLoader/></div>
+                    : renderCheckDateBox()
+                }
+              </div>
+              
               <p className="pavilion-registration-status__waiting-for-approval__text">
                 Ok! Now you have finished the process of registering your 
                 Pavilion for BB2020 (Bangkok Biennial 2020). What happens next:
