8fef499fe3f140686603123fb6f28ffc86da5aff
dodadoa <maddodadoa@gmail.com>
2021-01-03 20:15:09 +0700

    add back signup page for gatsby

---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../utils/layout'
+import SignUp from '../components/scenes/Auth/scenes/SignUp/SignUp'
+
+const SignInPage = () => (
+  <Layout>
+    <SignUp />
+  </Layout>
+)
+
+export default SignInPage
